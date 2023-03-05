387b29ed3488221d7eeb704907c8f6da8d6e1acf
Mendo6472 <mendo6472@gmail.com>
2023-03-04 20:59:56 -0500

    Added the add method to PointTree
    Added the add method to Point tree, changed the player class to also have a left and right player so the tree would work

--- src/model/Player.java
+++ src/model/Player.java
@@ -5,6 +5,8 @@ public class Player {
     private String name;
     private Player next;
     private double score;
+    private Player left;
+    private Player right;
 
     //Constructor
     public Player(String name){
@@ -32,6 +34,18 @@ public class Player {
     }
     public void setScore(double score) {
         this.score = score;
+    }
+    public Player getLeft() {
+        return left;
+    }
+    public void setLeft(Player left) {
+        this.left = left;
+    }
+    public Player getRight() {
+        return right;
+    }
+    public void setRight(Player right) {
+        this.right = right;
     }
 
     
--- src/model/PointTree.java
+++ src/model/PointTree.java
@@ -8,6 +8,38 @@ public class PointTree {
     public PointTree(){
         
     }
+
+    //Binary search tree methods
+
+    public void add(Player player){
+        if(root == null){
+            root = player;
+        }else{
+            add(root, player);
+        }
+    }
+    private void add(Player current, Player player){
+        if(player.getScore() < current.getScore()){
+          //Meter a la izquierda
+            if(current.getLeft() == null){
+                current.setLeft(player);
+            }else{
+                add(current.getLeft(), player);
+            }
+        }else if(player.getScore() > current.getScore()){
+            //Meter a la derecha
+            if(current.getRight() == null){
+                current.setRight(player);
+            }else{
+                add(current.getRight(), player);
+            }
+
+        }else{
+            //No hacer nada
+        }
+    }
+
+
     
     //-----Getters and setters-----
 
