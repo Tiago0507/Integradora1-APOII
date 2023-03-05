cb07ed8c2e455fa629354313f871fd1283819595
Mendo6472 <mendo6472@gmail.com>
2023-03-04 21:24:33 -0500

    Added the add method for PlayersList

--- src/model/PlayersList.java
+++ src/model/PlayersList.java
@@ -4,14 +4,30 @@ public class PlayersList {
 
     private Player head;
 
-    
+    //Methods
+
+    public void add(Player player){
+        if(head == null){
+            head = player;
+        }else{
+            add(head, player);
+        }
+    }
+    private void add(Player current, Player player){
+        if(current.getNext() == null){
+            current.setNext(player);
+            return;
+        }
+        add(current.getNext(), player);
+    }
+
+
     //-----Getters and setters-----
 
 
     public Player getHead() {
         return head;
     }
-
     public void setHead(Player head) {
         this.head = head;
     }
