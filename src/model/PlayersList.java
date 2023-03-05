514d99e00b1ae5942f895dc086aeeccde6164854
Mendo6472 <mendo6472@gmail.com>
2023-03-04 21:22:09 -0500

    Created the playerList class
    The player list will contain a list of the players that are currently in the box

---
+++ src/model/PlayersList.java
@@ -0,0 +1,19 @@
+package model;
+
+public class PlayersList {
+
+    private Player head;
+
+    
+    //-----Getters and setters-----
+
+
+    public Player getHead() {
+        return head;
+    }
+
+    public void setHead(Player head) {
+        this.head = head;
+    }
+
+}
